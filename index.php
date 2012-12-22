0d7aac10b20e14c4f4abc0b9a124895aff6c6a80
buschagne <itwerkzoekend@gmail.com>
2012-12-22 21:04:36 +0100

    php index page

---
+++ index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+
+    <title>jplay</title>
+
+    <link href="bootstrap/css/bootstrap.css" rel="stylesheet">
+
+    <link href="css/overcast/jquery-ui-1.9.2.custom.css" rel="stylesheet">
+    <link href="css/jquery-demo.css" rel="stylesheet">
+    <link href="css/my.css" rel="stylesheet">
+
+
+    <script src="js/jquery-1.8.3.js"></script>
+    <script src="js/jquery-ui-1.9.2.custom.js"></script>
+    <script src="js/jquery-demo.js"></script>
+    <script src="bootstrap/js/bootstrap.js"></script>
+
+    <script src="js/my.js"></script>
+
+  </head>
+  <body>
+    <div class="container">
+      <h1>Welcome to jPlay</h1>
+
+      <div class="hero-unit">
+        <h2>Hero unit</h2>
+        <p class="alert">TwitterBootstrap ready too!</p>
+        <a href="jquery-demo.html">JQuery Demo</a>
+
+        <!-- Button to trigger modal -->
+        <a href="#myModal" role="button" class="btn" data-toggle="modal">Launch demo modal</a>
+
+        <!-- Modal --> 
+        <div id="myModal" class="modal hide fade in" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+          <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal" aria-hidden="true">x</button>
+            <h3 id="myModalLabel">Modal header</h3>
+          </div>
+          <div class="modal-body">
+            <p>One fine body</p>
+            <input id="datepicker" type="text" name="date" />
+          </div>
+          <div class="modal-footer">
+            <button class="btn" data-dismiss="modal" aria-hidden="true">Close</button>
+            <button class="btn btn-primary">Save changes</button>
+          </div>
+        </div>
+
+
+
+      </div>
+
+
+
+
+    </div>
+  </body>
+</html>
